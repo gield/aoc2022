e507c8bb0fec024b9151bcbae0f7cf4acefad74e
Giel Dops <gieldops@hotmail.com>
2022-12-01 09:01:47 +0100

    Add day 1

---
+++ day01/solution1.py
@@ -0,0 +1,5 @@
+with open("input.txt", "r") as f:
+    lines = [l.strip() for l in f.readlines()]
+
+elves = [sum(map(int, elf.split("\n"))) for elf in "\n".join(lines).split("\n\n")]
+print(max(elves))
