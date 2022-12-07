f12fc679c0a2e70332048a1c04a5974255279eab
Giel Dops <gieldops@hotmail.com>
2022-12-07 21:33:51 +0100

    Add day 5

---
+++ day05/solution2.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+import re
+from typing import DefaultDict
+
+
+with open("input.txt", "r") as f:
+    lines = [l.rstrip() for l in f.readlines()]
+
+stacks: DefaultDict[int, list] = defaultdict(list)
+for l in lines[:lines.index("")]:
+    for i, j in enumerate(range(1, len(l), 4)):
+        crate = l[j:j + 1]
+        if crate == " " or crate.isnumeric():
+            continue
+        stacks[i + 1].insert(0, crate)
+
+for l in lines[lines.index("") + 1:]:
+    num, fr, to = map(int, re.findall(r"move (\d+) from (\d+) to (\d+)", l)[0])
+    stacks[to].extend(reversed([stacks[fr].pop() for _ in range(num)]))
+
+print("".join(stacks[i][-1] for i in sorted(stacks.keys())))
