f64ec402fa73aef41e0b9608adf813fb37f298d5
Giel Dops <gieldops@hotmail.com>
2022-12-14 19:38:45 +0100

    Add day 14

---
+++ day14/solution1.py
@@ -0,0 +1,31 @@
+from itertools import count
+
+
+with open("input.txt", "r") as f:
+    lines = [l.strip() for l in f.readlines()]
+
+source = (0, 500)
+grid = {source: "+"}
+for line in lines:
+    coords = [tuple(map(int, xy.split(","))) for xy in line.split(" -> ")]
+    for (fx, fy), (tx, ty) in zip(coords[:-1], coords[1:]):
+        for x in list(range(fx, tx + 1)) + list(range(tx, fx + 1)):
+            grid[(fy, x)] = "#"
+        for y in list(range(fy, ty + 1)) + list(range(ty, fy + 1)):
+            grid[(y, fx)] = "#"
+
+max_y = max(y for y, _ in grid.keys())
+for t in count():
+    y, x = source
+    while y <= max_y:
+        for dy, dx in [(1, 0), (1, -1), (1, 1)]:
+            if (y + dy, x + dx) not in grid:
+                y += dy
+                x += dx
+                break
+        else:
+            grid[(y, x)] = "O"
+            break
+    if y > max_y:
+        print(t)
+        break
